35c0c0cb41c6ab905c7848b39b3356bfc26eafcf
Bright Phakamad <bright.sp@hotmail.com>
2020-05-24 12:24:12 -0500

    created tags textfield

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="header">
+        <v-toolbar class="toolbar" dense :dark="darkMode">
+            <v-icon style="margin-right: 5px; color:#0077b6;">mdi-alpha-s-circle</v-icon>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="toggleMode(), $emit('changeMode', darkMode)" >
+                <v-icon>mdi-theme-light-dark</v-icon>
+            </v-btn>
+
+            {{onMode}}
+
+        </v-toolbar>
+    </div>  
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    export default Vue.extend({
+        name: "Header",
+        data() {
+            return {
+                title: "StackOverflow IDE",
+                darkMode: true,
+                onMode: "Dark Mode"
+            }
+        },
+        methods: {
+            toggleMode() {
+                this.darkMode = !this.darkMode
+                if (this.darkMode == true) {
+                    this.onMode = "Dark Mode";
+                    document.body.style.backgroundColor = "#252526";
+                } else {
+                    this.onMode = "Light Mode";
+                    document.body.style.backgroundColor = "white";
+                    
+                }
+            }
+        }
+    });
+</script>
+
+<style>  
+    .header {
+        margin-top: 20px;
+    }
+</style>
--- src/components/Query.vue
+++ src/components/Query.vue
@@ -1,48 +1,89 @@
 <template>
     <div>
-        <v-text-field
-            solo
-            autocomplete="off"
-            label="Enter Search Query"
-            v-model="query"
-            v-on:keyup.enter="queryStackExchange"
-            id="input-search"
-            append-icon="search"
-            @click:append="queryStackExchange"
-            >
-        </v-text-field>
-        <questions :results=results></questions>
+        <div class="query">
+            <v-row>
+                <!-- query -->
+                <v-col class="col-query">
+                    <v-text-field class="input-query"
+                        v-bind:class="{'theme--dark': darkMode}"
+                        filled
+                        dense
+                        autocomplete="off"
+                        placeholder="Enter Search Query"
+                        v-model="query"
+                        v-on:keyup.enter="queryStackExchange"
+                        id="input-search"
+                        append-icon="mdi-magnify"
+                        @click:append="queryStackExchange"
+                        >
+                    </v-text-field>
+                </v-col>
+                    <!-- tags -->
+                    <v-col class="col-tags">
+                        <v-text-field class="input-tags"
+                            v-bind:class="{'theme--dark': darkMode}"
+                            filled
+                            dense
+                            autocomplete="off"
+                            v-model="tags"
+                            v-on:keyup.enter="queryStackExchange"
+                            append-icon="mdi-code-tags"
+                            placeholder="Enter tags"
+                            >
+                        </v-text-field>
+                </v-col>
+                <!-- serach button -->
+                <v-col class="col-btn">
+                    <v-btn class="search-btn" 
+                        dark
+                        color="#0077b6"
+                        @click="queryStackExchange"
+                        >
+                        Search
+                    </v-btn>
+                </v-col>   
+            </v-row>
+        </div>
+        <div>
+            <questions :results=results></questions>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
     import Questions from "./Questions.vue";
+import { log } from "util";
 
     const axios = require("axios");
 
     export default Vue.extend({
         components: {
-            Questions
+            Questions,
         },
         props: [
-            "query"
+            "query",
+            "darkMode"
         ],
         data() {
             return {
                 results: [],
                 page: 1,
-                limit: 5
+                limit: 5,
+                tags: ""
             }
         },
         methods: {
             queryStackExchange() {
                 let _this = this;
+                var cleanTags = this.tags.trim().replace(new RegExp(" ", "g"), ";").replace(new RegExp(",", "g"), ";");
+                
                 axios.get(
                         "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&site=stackoverflow"
                         +"&page="+this.page
                         +"&pagesize="+this.limit
                         +"&q="+this.query
+                        +"&tagged="+cleanTags
                     ).then(function (response: any) {
                         _this.results = response.data.items
                     });
@@ -57,4 +98,21 @@
 </script>
 
 <style>
+
+    .query {
+        margin-top: 5px;
+    }
+
+    .col-query {
+        width: 100%;
+    }
+
+    .col-tags {
+        width: 100%;
+    }
+
+    .search-btn {
+        background-color: white;
+    }
+
 </style>
