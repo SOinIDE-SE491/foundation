d413f66a742c1e64a3209bc916da36b510f5c83a
Marko Milosavljevic <milosavljevic.marko@outlook.com>
2020-05-24 20:22:27 -0500

    Removed </v-simple-table> code.

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,21 +1,5 @@
 <template>
     <div>
-        <!-- <v-simple-table>
-            <template v-slot:default>
-            <thead>
-                <tr>
-                <th class="text-left">Questions</th>
-                <th class="text-left">Score</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in results" :key="item.question_id">
-                <td><a :href="item.link" >{{ item.title }}</a></td>
-                <td>{{ item.score }}</td>
-                </tr>
-            </tbody>
-            </template>
-        </v-simple-table> -->
         <hr id="hrTop">
         <div class="questionContainer" v-for="item in results" :key="item.question_id">
             <div class="questionStats">
